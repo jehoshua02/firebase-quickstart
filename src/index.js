da2cbe57aec0fffb5d077bebb87b918efedcdbc7
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-01-20 22:56:20 -0700

    Read and Write

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert('hello');
+var Firebase = require('firebase');
+var FirebaseRef = new Firebase('https://jehoshua02-qs.firebaseio.com/');
+
+FirebaseRef.set({
+  title: "Hello World!",
+  author: "Firebase",
+  location: {
+    city: "San Francisco",
+    state: "California",
+    zip: 94103
+  }
+});
+
+FirebaseRef.child("location/city").on("value", function(snapshot) {
+  alert(snapshot.val());  // Alerts "San Francisco"
+});
